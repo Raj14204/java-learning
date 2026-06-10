10e08b19641911a665d32e640e356d5e6bbf4fc7
Prathamesh Majalkar <majalkarprathamesh14@gmail.com>
2026-06-10 20:10:43 +0530

    inheritance

---
+++ Java-Learning/L_Inheritance.java
@@ -0,0 +1,11 @@
+public class L_Inheritance {
+
+    public static void main(String a[]) {
+        AdvCalc obj = new AdvCalc();
+        int r1 = obj.add(2, 2);
+        int r2 = obj.sub(2, 2);
+        int r3 = obj.mulitple(2, 2);
+        int r4 = obj.division(2, 2);
+        System.out.print(r1 + " " + r2 + " " + r3 + " " + r4 + " ");
+    }
+}
