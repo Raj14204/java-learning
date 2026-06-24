9b201030b70970d564d0404f7d163ae031025042
Prathamesh Majalkar <majalkarprathamesh14@gmail.com>
2026-06-24 22:37:10 +0530

    a

--- Java-Learning/L_Inheritance.java
+++ Java-Learning/L_Inheritance.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+
 public class L_Inheritance {
 
     public static void main(String a[]) {
@@ -7,5 +9,6 @@ public class L_Inheritance {
         int r3 = obj.mulitple(2, 2);
         int r4 = obj.division(2, 2);
         System.out.print(r1 + " " + r2 + " " + r3 + " " + r4 + " ");
+
     }
 }
