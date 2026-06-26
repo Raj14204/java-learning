69a09a329a47c633baf1be3df331b09e1c4aa1b9
Prathamesh Majalkar <majalkarprathamesh14@gmail.com>
2026-06-26 21:54:50 +0530

    Spring boot Web

---
+++ SpringBoot/SimpleWebApp/src/main/java/com/prathamesh/SimpleWebApp/HomeController.java
@@ -0,0 +1,19 @@
+package com.prathamesh.SimpleWebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+    @RequestMapping("/")
+public String great(){
+    return "Welcome to Prathamesh";
+}
+
+@RequestMapping("/about")
+public String about(){
+        return "About Page aa";
+}
+}
---
+++ SpringBoot/SimpleWebApp/src/main/java/com/prathamesh/SimpleWebApp/ProductController.java
@@ -0,0 +1,12 @@
+package com.prathamesh.SimpleWebApp;
+
+
+
+
+public class ProductController {
+
+
+public String getProducts(){
+return "";
+}
+}
