7199e8c1015a5098b6a2274f83780e385ca53dc4
Prathamesh Majalkar <majalkarprathamesh14@gmail.com>
2026-07-11 20:44:25 +0530

    spring jdbc

--- SpringBoot/SpringJDBCDemo/src/main/java/com/prathamesh/SpringJDBCDemo/SpringJdbcDemoApplication.java
+++ SpringBoot/SpringJDBCDemo/src/main/java/com/prathamesh/SpringJDBCDemo/SpringJdbcDemoApplication.java
@@ -1,13 +1,26 @@
 package com.prathamesh.SpringJDBCDemo;
 
+import com.prathamesh.SpringJDBCDemo.model.Alien;
+import com.prathamesh.SpringJDBCDemo.repo.AlienRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJdbcDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcDemoApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(SpringJdbcDemoApplication.class, args);
+		Alien alien1 = context.getBean(Alien.class);
+		alien1.setId(111);
+		alien1.setName("raj");
+		alien1.setTech("Java");
+
+		AlienRepo repo = context.getBean(AlienRepo.class); //obj
+		repo.save(alien1); //saving obj
+
+		System.out.print(repo.findAll());
 	}
 
 }
---
+++ SpringBoot/SpringJDBCDemo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id,name,tech) values (101,'kiran','blockchain');
+insert into alien (id,name,tech) values (102,'raj','web');
+insert into alien (id,name,tech) values (103,'kin','app');
